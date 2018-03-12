48420cbd936a14939bc95df411391790ef0013a2
Chris Yang <cyang287@yahoo.com>
2018-03-12 11:28:32 -0400

    more heroku testing

--- routes/users.js
+++ routes/users.js
@@ -16,106 +16,106 @@ router.get('/users', (req, res, next) => {
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
-// router.post('/users', (req, res, next) => {
-//   const {fullname, username, password} = req.body;
+router.post('/users', (req, res, next) => {
+  const {fullname, username, password} = req.body;
 
-//   const requiredFields = ['username', 'password'];
-//   const missingField = requiredFields.find(field => {
-//     !(field in req.body);
-//   });
-//   const nonTrimmedField = requiredFields.find(field => {
-//     req.body[field].trim() !== req.body[field];
-//   });
+  const requiredFields = ['username', 'password'];
+  const missingField = requiredFields.find(field => {
+    !(field in req.body);
+  });
+  const nonTrimmedField = requiredFields.find(field => {
+    req.body[field].trim() !== req.body[field];
+  });
 
-//   if (missingField) {
-//     return res.status(422).json({
-//       code: 422,
-//       reason: 'ValidationError',
-//       message: 'Missing field',
-//       location: missingField
-//     });
-//   }
-//   if (nonTrimmedField) {
-//     return res.status(422).json({
-//       code: 422,
-//       reason: 'ValidationError',
-//       message: 'Field cannot start or end with whitespace',
-//       location: nonTrimmedField
-//     });
-//   }
+  if (missingField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Missing field',
+      location: missingField
+    });
+  }
+  if (nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Field cannot start or end with whitespace',
+      location: nonTrimmedField
+    });
+  }
 
-//   const stringFields = ['username', 'password', 'fullname'];
-//   const nonStringField = stringFields.find(field => {
-//     field in req.body && typeof req.body[field] !== 'string';
-//   });
+  const stringFields = ['username', 'password', 'fullname'];
+  const nonStringField = stringFields.find(field => {
+    field in req.body && typeof req.body[field] !== 'string';
+  });
 
-//   if (nonStringField) {
-//     return res.status(422).json({
-//       code: 422,
-//       reason: 'ValidationError',
-//       message: 'Incorrect field type: expected string',
-//       location: nonStringField
-//     });
-//   }
+  if (nonStringField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Incorrect field type: expected string',
+      location: nonStringField
+    });
+  }
 
-//   const fieldSize = {
-//     username: {
-//       min: 1
-//     },
-//     password: {
-//       min: 8,
-//       max: 72
-//     }
-//   };
+  const fieldSize = {
+    username: {
+      min: 1
+    },
+    password: {
+      min: 8,
+      max: 72
+    }
+  };
 
-//   const tooSmallField = Object.keys(fieldSize).find(field => {
-//     'min' in fieldSize[field] && 
-//       req.body[field].length >= fieldSize[field].min;
-//   });
+  const tooSmallField = Object.keys(fieldSize).find(field => {
+    'min' in fieldSize[field] && 
+      req.body[field].length >= fieldSize[field].min;
+  });
 
-//   const tooLargeField = Object.keys(fieldSize).find(field => {
-//     'max' in fieldSize[field] &&
-//       req.body[field].length <= fieldSize[field].max;
-//   });
+  const tooLargeField = Object.keys(fieldSize).find(field => {
+    'max' in fieldSize[field] &&
+      req.body[field].length <= fieldSize[field].max;
+  });
 
-//   if (tooSmallField) {
-//     return res.status(422).json({
-//       code: 422,
-//       reason: 'ValidationError',
-//       message: `Username or Password needs to be at least 
-//       ${fieldSize[tooSmallField].min} characters long`,
-//       location: tooSmallField
-//     });
-//   }
+  if (tooSmallField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: `Username or Password needs to be at least 
+      ${fieldSize[tooSmallField].min} characters long`,
+      location: tooSmallField
+    });
+  }
 
-//   if (tooLargeField) {
-//     return res.status(422).json({
-//       code: 422,
-//       reason: 'ValidationError',
-//       message: `Username or Password needs to be at most 
-//       ${fieldSize[tooLargeField].max} characters long`,
-//       location: tooLargeField
-//     });
-//   }
+  if (tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: `Username or Password needs to be at most 
+      ${fieldSize[tooLargeField].max} characters long`,
+      location: tooLargeField
+    });
+  }
 
-//   return User.hashPassword(password)
-//     .then(digest => {
-//       const newUser = {
-//         username,
-//         password: digest,
-//         fullname
-//       };
-//       return User.create(newUser);
-//     })
-//     .then(result => {
-//       return res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
-//     })
-//     .catch(err => {
-//       if (err.code === 11000) {
-//         err = new Error('The username already exists');
-//       }
-//       next(err);
-//     });
-// });
+  return User.hashPassword(password)
+    .then(digest => {
+      const newUser = {
+        username,
+        password: digest,
+        fullname
+      };
+      return User.create(newUser);
+    })
+    .then(result => {
+      return res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The username already exists');
+      }
+      next(err);
+    });
+});
 
 module.exports = router;
